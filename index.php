0736884134b1a3d4e398061f9c9d10ee02981527
20071708-tech <20071708@clases.edu.sv>
2026-05-19 08:22:52 -0600

    Add files via upload
    repositorio que contiene la guia de ejercicios

---
+++ index.php
@@ -0,0 +1,93 @@
+<html>
+    <head>
+        <title>repositorio</title>
+    </head>
+    <body>
+        <h1>GUIA DE EJERCICIOS</h1>
+
+        <a href="formulario.html"> ejercicio 1 </a>
+        <p>mostrar un mensaje de bienvenida</p>
+        <br>
+        <a href="formulario de suma.html" > ejercicio 2 </a>
+        <p>dos numeros que muestren el resultado de una suma</p>
+        <br>
+        <a href="formulario de resta.html" > ejercicio 3 </a>
+        <p>ingresar dos numeros que muestre el resultado de una resta</p>
+        <br>
+        <a href="formulario de multiplicacion.html" > ejercicio 4 </a>
+        <p>ingresar dos numeros que muestre el resultado de una multiplicacion</p>
+        <br>
+        <a href="formulario de resta.html" > ejercicio 5 </a>
+        <p>ingresar dos numeros que muestre el resultado de una resta</p>
+        <br>
+        <a href="nombre y edad.html" > ejercicio 6 </a>
+        <p>se muestra el nombre y la edad</p>
+        <br>
+        <a href="dos numeros.html" > ejercicio 7 </a>
+        <p>dos numeros y se muestra el numero mayor</p>
+        <br>
+        <a href="formulario de dos numeros.html" > ejercicio 8 </a>
+        <p>dos numeros y se muestra numero menor</p>
+        <br>
+        <a href="numero entero.html" > ejercicio 9 </a>
+        <p>un numero entero y se mostrara si es par o impar</p>
+        <br>
+        <a href="formulario de notas.html" > ejercicio 10</a>
+        <p>tres notas y muestra el promedio</p>
+        <br>
+        <a href="datos del estudite.html" > ejercicio 11 </a>
+        <p>tres notas y se mostrara si aprobo o reprobo</p>
+        <br>
+        <a href="rectangulo.html" > ejercicio 12 </a>
+        <p>base y altura de un rectangulo y calcularlo</p>
+        <br>
+        <a href="valor de un lado de un cuadrado.html" > ejercicio 13 </a>
+        <p>el valor de un lado de un cuadrado y calcular el perimetro</p>
+        <br>
+        <a href="ingresar cantidad.html" > ejercicio 14 </a>
+        <p>ingresar cantidad en metros y la muestre la cantidad en centimetros</p>
+        <br> 
+        <a href="grados celsius.html" > ejercicio 15 </a>
+        <p>formulario que solicite una temperatura en grados Celsius</p>
+        <br>
+        <a href="formulario del empleado.html" > ejercicio 16 </a>
+        <p>nombre del empleado las horas trabajadas pero debo mostrar el calculo del salario</p>
+        <br>
+        <a href="monto total.html" > ejercicio 7 </a>
+        <p>dos numeros y se muestra el mayor</p>
+        <br>
+        <a href="cliente.html" > ejercicio 18 </a>
+        <p>nombre del cliente la cantidad y el precio</p>
+        <br>
+        <a href="tabla de multiplicar.html" > ejercicio 19 </a>
+        <p>un numero y se muestre la tabla de multiplicar del 1 al 10</p>
+        <br>
+        <a href="formulario de ingresar un numero.html" > ejercicio 20 </a>
+        <p>ingresar un numero y que se realice la suma de los numeros desde 1 hasta el numero ingresado</p>
+        <br>
+        <a href="palabra.html" > ejercicio 21 </a>
+        <p>una palabra donde muestre la cantidad de letras </p>
+        <br>
+        <a href="cantidad en dolares.html" > ejercicio 22</a>
+        <p>cantidad en dolares pero se realizara la convencion en euros</p>
+        <br>
+        <a href="formulario de precio y producto.html" > ejercicio 23 </a>
+        <p>formulario que que solicite el precio y el producto</p>
+        <br>
+        <a href="edad de personal.html" > ejercicio 24 </a>
+        <p>formulario que solicite la edad actual de una persona</p>
+        <br>
+        <a href="formulario del usuario.html" > ejercicio 25 </a>
+        <p>un formulario que solicite nombre, correo electrónico y ciudad del usuario.</p>
+        <br>
+
+
+
+        
+        
+
+
+
+
+    </body> 
+</html>
