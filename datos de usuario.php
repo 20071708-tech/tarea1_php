9ed5274e0ed39f2d7028a010774a1cc4b6cc042f
20071708-tech <20071708@clases.edu.sv>
2026-05-19 14:24:17 -0600

    Add files via upload
    ejercicio 25:
    Diseñe un formulario que solicite nombre, correo electrónico y ciudad del usuario.
    Los datos deben enviarse a otro archivo PHP donde se muestre toda la
    información ingresada.

---
+++ datos de usuario.php
@@ -0,0 +1,21 @@
+<?php
+
+$nombre = $_POST["nombre"];
+
+$correo = $_POST["correo"];
+
+$ciudad = $_POST["ciudad"];
+
+echo "<h2>Información ingresada</h2>";
+
+echo "Nombre: " . $nombre;
+
+echo "<br><br>";
+
+echo "Correo electrónico: " . $correo;
+
+echo "<br><br>";
+
+echo "Ciudad: " . $ciudad;
+
+?>
